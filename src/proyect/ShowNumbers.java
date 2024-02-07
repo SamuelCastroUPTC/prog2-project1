0f6df763986ae11c2d0a862f6f8fed8ee4f13b15
unknown <castro.samuel@uptc.edu.co>
2024-02-07 11:08:19 -0500

    udpate shoe number

--- src/proyect/ShowNumbers.java
+++ src/proyect/ShowNumbers.java
@@ -3,7 +3,7 @@ package proyect;
 
 public class ShowNumbers {
     public void numbers(){
-        for(int i=0; i<1001; i++){
+        for(int i=100; i<200; i++){
         System.out.println(i);
         }
     }
